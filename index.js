9ddc47c1d2c88277403063d7c5df4ec8fabfb65d
deepakkumar29333 <deepakkumar@bitqit.com>
2023-01-02 12:44:22 +0530

    short code

--- index.js
+++ index.js
@@ -7,43 +7,20 @@
     show(data);
 })()
 
-async function getApiFilter(filterBy, min, max) {
+async function getApiFilter(filterBy, min, max,searchBy) {
     DeleteRows();
     let gt = filterBy + "_gt";
     let lt = filterBy + "_lt";
+    let beerName="beer_name";
+    if(searchBy==""){
+        beerName="bear_name";
+    }
     min = parseInt(min);
     max = parseInt(max);
-    const response = await fetch(`https://api.punkapi.com/v2/beers?${gt}=${min}&${lt}=${max}`);
+    const response = await fetch(`https://api.punkapi.com/v2/beers?${gt}=${min}&${lt}=${max}&${beerName}=${searchBy}`);
     let data = await response.json();
     console.log(data);
     show(data);
-}
-async function getApi(searchBy) {
-    DeleteRows();
-    const response = await fetch(`https://api.punkapi.com/v2/beers?beer_name=${searchBy}`);
-    let data = await response.json();
-    console.log(data);
-    show(data);
-}
-//table row removing function
-function DeleteRows() {
-    var rowCount = table_body.rows.length;
-    for (var i = rowCount - 1; i > 0; i--) {
-        table_body.deleteRow(i);
-    }
-}
-
-//sort by
-function filterData() {
-    let filerBy = document.getElementById("filter-by").value;
-    let min = document.getElementById("min").value;
-    let max = document.getElementById("max").value;
-    getApiFilter(filerBy, min, max);
-}
-//filter by name
-function filterByName(){
-    let searchBy = document.getElementById("search-by-name").value;
-    getApi(searchBy);
 }
 function show(data) {
     let tab = "";
@@ -62,4 +39,40 @@ function show(data) {
         tableRef.insertRow().innerHTML = tab;
     }
 }
+function filterData() {
+    let filerBy = document.getElementById("filter-by").value;
+    let min = document.getElementById("min").value;
+    let max = document.getElementById("max").value;
+    let searchBy = document.getElementById("search-by-name").value;
+    getApiFilter(filerBy, min, max,searchBy);
+}
+
+
+/*async function getApi(searchBy) {
+    DeleteRows();
+    const response = await fetch(`https://api.punkapi.com/v2/beers?beer_name=${searchBy}`);
+    let data = await response.json();
+    console.log(data);
+    show(data);
+}
+
+//table row removing function
+*/
+function DeleteRows() {
+    let rowCount = table_body.rows.length;
+    for (let i = rowCount - 1; i > 0; i--){
+        table_body.deleteRow(i);
+    }
+}
+/*
+//sort by
+
+
+//filter by name
+/*
+function filterByName() {
+    let searchBy = document.getElementById("search-by-name").value;
+    getApi(searchBy);
+}
+*/
 
