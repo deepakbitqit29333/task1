3f02fee6d90361e39ce2ed3739aff8e7af34e7fa
deepakkumar29333 <deepakkumar@bitqit.com>
2023-01-02 18:59:00 +0530

    completed 1.1

--- index.js
+++ index.js
@@ -1,39 +1,15 @@
-async function getApi() {
-    // Storing response
-    const response = await (await fetch(`https://api.punkapi.com/v2/beers`)).json();
-    // Storing data in form of JSON
-    // let data = response.json();
-    console.log("data",response);
-    showRecord(response);
-}
-
-getApi();
-
 
 // todo: use a variable baseUrl and add the parameters according to it. //done
 // todo: rename variables //done
-// todo: use a common function to fetch records.
-async function getApiFilter(abvLt, abvGt, ibuLt, ibuGt, searchBy) {
-    await DeleteRows();
-    let url=`https://api.punkapi.com/v2/beers?`;
-    if(searchBy){
-        url+=`beer_name=${searchBy}&`;
-    }
-    url+=`abv_gt=${abvLt}&abv_lt=${abvGt}&ibu_lt=${ibuGt}&ibu_gt=${ibuLt}`;
-    // todo: can convert in single line.
-    const response = await fetch(url);
-    let data = await response.json();
-    console.log(data);
-    showRecord(data);
-}
-// todo: rename data to rows
-function showRecord(data) {
+// todo: use a common function to fetch records. //done
+// todo: rename data to rows //done
+function showRecord(rows) {
     let tableRow = "";
     // Loop to access all rows
     let tableRef = document.getElementById('table_body')
         .getElementsByTagName('tbody')[0];
     // todo: rename "r" // done
-    for (let row of data) {
+    for (let row of rows) {
         tableRow = `<tr> 
                 <td>${row.id} </td>
                 <td>${row.name}</td>
@@ -46,53 +22,57 @@ function showRecord(data) {
     }
 }
 
-async function filterData() {
-    // let abv = document.getElementsByName("abv").value;
+async function filterData(){
+    let url=`https://api.punkapi.com/v2/beers?`;
     let abv = document.querySelector('input[name="abv"]:checked');
-    // todo: if (abv)  // done
-    // todo: remove hard coded values.
-    if (abv) {
-        abv = abv.value;
-    } else {
-        abv = "0-100";
+    if(abv){
+        abv=abv.value;
+        let abvArray = abv.split("-");
+        if(abvArray[0]){
+            url+=`abv_gt=${abvArray[0]}&`;
+        }
+        if(abvArray[1]){
+            url+=`abv_lt=${abvArray[1]}&`
+        }
     }
-    let abvArray = abv.split("-");
-    let ibu = document.querySelector('input[name="ibu"]:checked');
-    if (ibu) {
-        ibu = ibu.value;
-    } else {
-        ibu = "0-100";
-    }
-    let ibuArray = ibu.split("-");
-    let searchBy = document.getElementById("search-by-name").value;
-    await getApiFilter(abvArray[0], abvArray[1], ibuArray[0], ibuArray[1], searchBy);
-}
 
+    // todo: if (abv)  // done
+    // todo: remove hard coded values. // done
+    let ibu = document.querySelector('input[name="ibu"]:checked');
+    if(ibu){
+        ibu=ibu.value;
+        let ibuArray = ibu.split("-");
+        if(ibuArray[0]){
+            url+=`ibu_gt=${ibuArray[0]}&`;
+        }
+        if(ibuArray[1]){
+            url+=`ibu_lt=${ibuArray[1]}&`;
+        }
+    }
+    let beerName = document.getElementById("search-by-name").value;
+    if(beerName){
+        url+=`beer_name=${beerName}&`;
+    }
+   await DeleteRows();
+    const response = await (await fetch(url)).json();
+    showRecord(response);
+}
+filterData();
 async function resetFilter() {
     let abv = document.querySelector('input[name="abv"]:checked');
     if (abv) {
         abv.checked = false;
     }
     let ibu = document.querySelector('input[name="ibu"]:checked');
-    if (abv){
+    if (ibu){
         ibu.checked = false;
     }
     let searchBy = document.getElementById("search-by-name").value = "";
     await DeleteRows();
-    await getApi();
+    await filterData();
 }
 
 
-/*async function getApi(searchBy) {
-    DeleteRows();
-    const response = await fetch(`https://api.punkapi.com/v2/beers?beer_name=${searchBy}`);
-    let data = await response.json();
-    console.log(data);
-    show(data);
-}
-
-//table row removing function
-*/
  async function DeleteRows() {
     let rowCount = table_body.rows.length;
     for (let i = rowCount - 1; i > 0; i--) {
@@ -100,15 +80,4 @@ async function resetFilter() {
     }
 }
 
-/*
-//sort by
-
-
-//filter by name
-/*
-function filterByName() {
-    let searchBy = document.getElementById("search-by-name").value;
-    getApi(searchBy);
-}
-*/
 
